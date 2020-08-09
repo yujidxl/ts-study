a64ad080acbd099f5ec202d9dec729b6be82cb6d
dengxiaolong <dengxiaolong@myhexin.com>
2020-08-09 19:04:44 +0800

    fixed元组使用错误

--- classify/index.ts
+++ classify/index.ts
@@ -8,8 +8,11 @@ let arr: number[] = [1, 2, 3];
 let x: [string, number] = ['name', 1];
 console.log(x[0].substr(1)); // OK
 console.log(x[1].substr(1)); // Error, 'number' does not have 'substr'
-x[3] = 'world'; // OK, 字符串可以赋值给(string | number)类型
-console.log(x[5].toString()); // OK, 'string' 和 'number' 都有 toString
+// ts2.7后不支持访问超出元组长度的元素了，所以这种及后面的都会报错
+// error, 字符串可以赋值给(string | number)类型 
+x[3] = 'world'; 
+// error, 'string' 和 'number' 都有 toString
+console.log(x[5].toString());
 x[6] = true; // Error, 布尔不是(string | number)类型
 
 // 枚举enum
